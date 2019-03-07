6faec35e603edc0734a267542449b3f85b545966
new2scala <jiajiwu.icare@gmail.com>
2019-03-07 15:08:28 +0100

    end example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Model } from "./model";
+import { Model, TodoItem } from "./model";
 
 @Component({
   selector: 'todo-app',
@@ -13,6 +13,14 @@ export class AppComponent {
     return this.model.user;
   }
   getTodoItems() {
-    return this.model.items;
+    return this.model.items.filter(item => !item.done);
+  }
+
+  addItem(newItem) {
+    if (newItem != "") {
+      this.model.items.push(
+        new TodoItem(newItem, false)
+      )
+    }
   }
 }
